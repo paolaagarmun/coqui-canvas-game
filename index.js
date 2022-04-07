9e3a41a28824be538c07f15c15f205619e1186de
paolaagarmun <paola.a.garciamunoz@gmail.com>
2022-04-07 18:03:15 -0400

    working on background img

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
+document.getElementById("start-button").onclick = () => {
+  startGame();
+};
+
 const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext("2d");
 
@@ -6,11 +10,10 @@ canvas.height = 550;
 
 let w = canvas.width;
 let h = canvas.height;
-
+let gameOn = false;
 
 const coquiMacho = new Coqui(50, 50, 30, 30, "brown");
 const coquiHembra = new Coqui(w - 100, h - 100, 30, 30, "green");
-
 
 document.addEventListener("keydown", (e) => {
   switch (e.code) {
@@ -26,51 +29,59 @@ document.addEventListener("keydown", (e) => {
 });
 
 document.addEventListener("keyup", (e) => {
-    coquiHembra.speedX = 0;
-    coquiHembra.speedY = 0;
-  });
+  coquiHembra.speedX = 0;
+  coquiHembra.speedY = 0;
+});
 
-let ramaId =0;
+let ramaId = 0;
 let platforms = [];
 function createPlatforms() {
-    platforms.push(new Ramas(ramaId));
-    ramaId++;
+  platforms.push(new Ramas(ramaId));
+  ramaId++;
 }
-
 
 const startGame = () => {
+  if (!gameOn) {
+    gameOn = true;
     setInterval(createPlatforms, 800);
     animate();
-}
+  }
+};
 
-let int
+let int;
+const gameOver = () => {
+  window.cancelAnimationFrame(int);
+  ctx.clearRect(0, 0, w, h);
+  gameOn = false;
+  document.getElementById("start-button").onclick = () => {
+    startGame();
+  };
+};
+
 const animate = () => {
   int = window.requestAnimationFrame(animate);
   ctx.clearRect(0, 0, w, h);
 
-  
   coquiHembra.draw();
   coquiMacho.draw();
   coquiHembra.gravity();
- 
+
   coquiHembra.move();
-  
+
   platforms.forEach((platform) => {
     platform.draw();
     let didCollide = collisionDetection(coquiHembra, platform);
-    if(didCollide) {
-        coquiHembra.y = platform.y - coquiHembra.height
+    if (didCollide) {
+      coquiHembra.y = platform.y - coquiHembra.height;
+
     }
   });
-  let didCollideWithCoquiMacho = collisionDetection(coquiHembra,coquiMacho);
+  let didCollideWithCoquiMacho = collisionDetection(coquiHembra, coquiMacho);
   if (didCollideWithCoquiMacho) {
-      coquiHembra.x = coquiMacho.x + coquiMacho.width
-      coquiHembra.y = coquiMacho.y
-      window.cancelAnimationFrame(int)
+    coquiHembra.x = coquiMacho.x + coquiMacho.width;
+    coquiHembra.y = coquiMacho.y;
+    gameOver();
   }
-
-
-
 };
 
 function collisionDetection(player, object) {
@@ -80,10 +91,8 @@ function collisionDetection(player, object) {
     player.y < object.y + object.height &&
     player.y + player.height > object.y
   ) {
-    return true
+    return true;
   } else {
-      return false
+    return false;
   }
 }
-
-startGame();
